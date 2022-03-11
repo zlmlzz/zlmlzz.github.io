5e0c3bf9b896d80e4302a63a4127a2d26568f99e
zlmlzz <2327567698@qq.com>
2022-03-11 14:46:20 +0800

    publish style

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,9 +106,19 @@ $(function() {
       if (online()) {
         // 调用发表评论接口
       } else {
-        // 重定向登录地址
+        window.location.href = home + "/login.html";
       }
     });
+
+    // 回复
+    $(document).on("click", ".send", function () {
+      if (online()) {
+        // 调用回复接口
+      } else {
+        window.location.href = home + "/login.html";
+      }
+    });
+
 
     // 注册
     $('#register').on("click", function () {
@@ -236,6 +246,114 @@ $(function() {
     }
   }
 
+  function list() {
+    var data = {
+      "size": 281,
+      "list": [{
+        "id": 1,
+        "imageUrl": "https://himg.bdimg.com/sys/portrait/item/wise.1.27fe2bb5.7hXDYeTK0d3OIahW6MztlA.jpg?time=7402",
+        "nickName": "zhangnan1111ai",
+        "time": "1小时前",
+        "likeText": 1948,
+        "content": "然而没有，都是缓兵之计，一个等白俄罗斯和车臣增兵，一个等国际雇佣兵登场。",
+        "child": [{
+          "id": 2,
+          "imageUrl": "https://himg.bdimg.com/sys/portrait/item/wise.1.eec5bf39.edtuVzcQyQNNohDE2HIdlg.jpg?time=3635",
+          "nickName": "蓝浩洋",
+          "time": "14分钟前",
+          "content": "斯拉夫",
+          "likeText": "赞"
+        }]
+      }]
+    };
+
+    if (data && data.size > 0) {
+      var html = '<h2 class="xcp-list-title">评论列表(' + data.size + '条)</h2>'
+          + '        <div class="xcp-list-list">';
+      for (var i = 0; i < data.list.length; i++) {
+        var item = data.list[i];
+        html = html +
+            '            <div class="xcp-item">' +
+            '<input hidden="hidden" value="' + item.id + '">' +
+            '                <div class="left">' +
+            '                    <div class="x-avatar">' +
+            '                        <div class="x-avatar-placeholder"></div>' +
+            '                        <div class="x-avatar-img"' +
+            '                             style="background-image: url(' + item.imageUrl + ')"></div>' +
+            '                    </div>' +
+            '                </div>' +
+            '                <div class="right">' +
+            '                    <div class="user-bar">' +
+            '                        <h5 class="user-bar-uname">' + item.nickName + '</h5>' +
+            '                    </div>' +
+            '                    <div class="x-interact-rich-text rich-text">' +
+            '                        <span class="type-text">' + item.content + '</span>' +
+            '                    </div>' +
+            '                    <div class="interact-bar">' +
+            '                        <div class="interact-bar-left">' +
+            '                            <span class="time">' + item.time + '</span>' +
+            '                            <span class="report">举报</span>' +
+            '                        </div>' +
+            '                        <div class="interact-bar-right">' +
+            '                            <div class="reply">' +
+            '                                <i class="icon reply-icon"></i>' +
+            '                                <span class="reply-text">回复</span>' +
+            '                            </div>' +
+            '                            <div class="like ">' +
+            '                                <i class="icon like-icon"></i>' +
+            '                                <span class="like-text">' + item.likeText + '</span>' +
+            '                            </div>' +
+            '                        </div></div>';
+        if (item && item.child && item.child.length > 0) {
+          html = html + '                    <div class="xcp-list">' +
+              '                        <div class="xcp-list-list is-second">';
+          for (var j = 0; j < item.child.length; j++) {
+            var child = item.child[j];
+            html = html +
+                '                            <div class="xcp-item">' +
+                '<input hidden="hidden" value="' + child.id + '">' +
+                '                                <div class="left">' +
+                '                                    <div class="x-avatar is-small">' +
+                '                                        <div class="x-avatar-placeholder"></div>' +
+                '                                        <div class="x-avatar-img"' +
+                '                                             style="background-image: url(' + child.imageUrl + ')"></div>' +
+                '                                    </div>' +
+                '                                </div>' +
+                '                                <div class="right">' +
+                '                                    <div class="user-bar">' +
+                '                                        <h5 class="user-bar-uname">' + child.nickName + '</h5>' +
+                '                                    </div>' +
+                '                                    <div class="x-interact-rich-text rich-text"' +
+                '                                         >' +
+                '                                        <span class="type-text">' + child.content + '</span>' +
+                '                                    </div>' +
+                '                                    <div class="interact-bar">' +
+                '                                        <div class="interact-bar-left">' +
+                '                                            <span class="time">' + child.time + '</span>' +
+                '                                            <span class="report">举报</span>' +
+                '                                        </div>' +
+                '                                        <div class="interact-bar-right">' +
+                '                                            <div class="reply">' +
+                '                                                <i class="icon reply-icon"></i>' +
+                '                                            </div>' +
+                '                                            <div class="like ">' +
+                '                                                <i class="icon like-icon"></i>' +
+                '                                                <span class="like-text">' + child.likeText + '</span>' +
+                '                                            </div>' +
+                '                                        </div>' +
+                '                                    </div>' +
+                '                                </div>' +
+                '                            </div>';
+          }
+          html = html + '</div></div>';
+        }
+        html = html + '</div>';
+      }
+      html = html + '</div>';
+      $('.xcp-list').append(html);
+    }
+  }
+
   function adjust(online) {
     if (online) {
       $('#login-btn').hide();
@@ -332,4 +450,32 @@ $(function() {
   });
 
   online();
+  list();
+  // 回复
+  $('.reply-text').on("click", function () {
+    var val = $(this).text();
+    var interactBar = $(this).parents('.interact-bar');
+    if ("回复" === val) {
+      $(this).text("取消回复");
+      var text = interactBar.prevAll('.user-bar').text();
+      var html = '<div style="padding-top: 17px;">' +
+          '            <div class="x-interact-publish">' +
+          '                <div class="x-interact-publish-cont">' +
+          '                    <textarea contenteditable="true" class="text-area placeholder" placeholder="回复' + text.trim() + ':"></textarea>' +
+          '                    <div class="other"></div>' +
+          '                </div>' +
+          '                <div class="x-interact-publish-panel-center">' +
+          '                </div>' +
+          '                <div class="x-interact-publish-opt ">' +
+          '                    <span class="send enable">发表</span>' +
+          '                </div>' +
+          '            </div>' +
+          '        </div>';
+      interactBar.after(html);
+    } else if ("取消回复" === val) {
+      $(this).text("回复");
+      interactBar.next().remove();
+    }
+  });
+
 });
